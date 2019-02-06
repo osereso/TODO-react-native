27ca1c3fd92b0666e10961d7358a83cb20902d2d
mbp <mbp@MBP-15-srs.local>
2019-02-06 17:24:14 +0100

    delete notes

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,7 +4,25 @@ import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity} from '
 import Note from './Note';
 
 export default class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteArray: [],
+      noteText: '',
+    }
+  }
+
   render() {
+
+    let notes = this.state.noteArray.map((val, key ) => {
+      return <Note 
+              key={key} 
+              keyval={key} 
+              val={val}
+              deleteMethod={() => this.deleteNote(key)} />
+    });
+
     return (
       <View style={styles.container}>
           <View style={styles.header}>
@@ -13,17 +31,22 @@ export default class Main extends Component {
             </Text>
           </View>
           <ScrollView style={styles.scrollContainer}>
+            {notes}
           </ScrollView>
           <View style={styles.footer}>
             <TextInput 
               style={styles.textInput}
+              onChangeText={(noteText) => this.setState({noteText})}
+              value={this.state.noteText}
               placeHolder=">note"
               placeholderTextColor="white"
               underlineColorAndroid="transparent">
             </TextInput>
           </View>
           
-          <TouchableOpacity style={styles.addButton}>
+          <TouchableOpacity 
+            onPress={ this.addNote.bind(this) }
+            style={styles.addButton}>
             <Text style={styles.addButtonText}>
               +
             </Text>
@@ -32,11 +55,34 @@ export default class Main extends Component {
       </View>
     );
   }
+
+  addNote() {
+
+    if (this.state.noteText) {
+
+      var d = new Date();
+      this.state.noteArray.push({
+        'date': d.getFullYear() + 
+        "/" + (d.getMonth() + 1) +
+        "/" + d.getDate(),
+        'note' : this.state.noteText
+      });
+      this.setState({ noteArray: this.state.noteArray })
+      this.setState({ noteText: '' })
+    }
+  }
+
+  deleteNote(key) {
+    this.state.noteArray.splice(key, 1);
+    this.setState({ noteArray: this.state.noteArray })
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white'
   },
 
   header: {
