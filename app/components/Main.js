d3eaa911897f208bc88dd3125c5acb2aaa00e885
mbp <mbp@MBP-15-srs.local>
2019-02-06 14:39:57 +0100

    note file with new entries and style

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity} from 'react-native';
 
+import Note from './Note';
+
 export default class Main extends Component {
   render() {
     return (
